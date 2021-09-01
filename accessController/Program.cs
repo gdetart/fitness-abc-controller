4ec869e73648e53d6d6cd4679d204a077686a5a5
unknown <detartgrajcevci96@gmail.com>
2021-09-01 19:02:08 +0100

    k

--- accessController/Program.cs
+++ accessController/Program.cs
@@ -4,6 +4,7 @@ namespace accessController
 {
     static class Program
     {
+        [STAThread]
         static void Main()
         {
             Controller cont = new Controller ("192.168.0.121",  223211117 );
